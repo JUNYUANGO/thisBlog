de70f2d78488b8ee2fb87dab5c98f1bcd86baf49
bread_d <wang9747@umn.edu>
2024-01-30 22:18:13 -0600

    Initial commit

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,14 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
+	// author: 'DanielCG', // Site author
+	author: 'Junyuan Wang',
+	// title: 'Astro Theme OpenBlog', // Site title.
+	title: 'ThisBlog',
+	// description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	description: 'Blogs Only Here!',
+	lang: 'en-us',
+	ogLocale: 'en_US',
 	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
