063077168a551e5447d3abe366d67b91339987be
bread_d <wang9747@umn.edu>
2024-02-05 17:12:28 -0600

    Update new articles, modify categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,10 +1,8 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5',
-	'Introduction',
-	'Personal'
+	'Computer Science',
+	'Psychology',
+	'Lifelong Learning',
+	'Personal',
+	'About'
 ] as const
